8edd3f26bb619c9790b9553cef8d8b4fb9e7ce90
Raja Raman <rraman@sapient.com>
2016-12-24 08:58:22 +0530

    javascript - initial learning files

---
+++ javascript/javascriptbasics/DOM/AccessingDOM/script.js
@@ -0,0 +1,5 @@
+var mainContent = document.getElementById("mainContent");
+mainContent.setAttribute("align", "right");
+var mainTitle = document.getElementById("mainTitle");
+mainTitle.innerHTML = "Raja changed title!";
+console.log(mainTitle.innerHTML);
