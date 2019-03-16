917bdebae0e9044aa3c3f7dec44c14dbcf990725
rajraman <mmidtm>
2019-03-16 07:54:26 +0530

    jira api play

---
+++ python/jiraplay.py
@@ -0,0 +1,15 @@
+from atlassian import Jira
+
+def getSummary(issue):
+    return issue['fields']['summary']
+
+jira = Jira(
+    url='https://tools.publicis.sapient.com/jira',
+    username='rajraman',
+    password='##')
+
+JQL = 'project in (WGT) and priority in ("P1 - Blocker") and status not in (Closed)'
+data = jira.jql(JQL)
+# Print the first issue summary
+for issue in data['issues']:
+    print(getSummary(issue))
