03b19b96753222cf7e80a1c3621f82bcf2e9ca05
Raja Raman <rajraman@publicisgroupe.net>
2019-08-04 07:59:14 +0530

    Added hellow world code for rasa

--- chatbots/rasahelloworld/rasa-nlu.py
+++ chatbots/rasahelloworld/rasa-nlu.py
@@ -1,13 +1,14 @@
-from rasa_nlu.training_data import load_data
-from rasa_nlu.model import Trainer
-from rasa_nlu import config
-from rasa_nlu.model import Interpreter
+from rasa.nlu.training_data import load_data
+from rasa.nlu.model import Trainer
+from rasa.nlu import config
+from rasa.nlu.model import Interpreter
 def train_horoscopebot(data_json, config_file, model_dir):
     training_data = load_data(data_json)
     trainer = Trainer(config.load(config_file))
     trainer.train(training_data)
     model_directory = trainer.persist(model_dir, fixed_model_name = 'horoscopebot')
 def predict_intent(text):
-    interpreter = Interpreter.load('./models/nlu/default/horoscopebot')
+    interpreter = Interpreter.load('./models/nlu/horoscopebot')
     print(interpreter.parse(text))
-train_horoscopebot('./data/data.json', 'config.json', './models/nlu')
+#train_horoscopebot('./data/data.json', 'config.json', './models/nlu')
+predict_intent("I am looking for my horoscope for today. I am wondering if you can tell me that.")
