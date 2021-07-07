3f17e28ab765e86444d7f5f532ab5cbd2093a47c
Raja Raman <raman.raja@gmail.com>
2021-07-07 12:40:58 +0530

    Added missing file!

---
+++ java/springbootsamples/sleuth/block/src/main/java/com/raja/BlockApplication.java
@@ -0,0 +1,22 @@
+package com.raja;
+
+import brave.sampler.Sampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class BlockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockApplication.class, args);
+    }
+}
+
